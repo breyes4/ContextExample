bfe182e2233996ba9e198aff86024d5f78f51ddc
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-11-02 17:42:11 -0500

    MainService.cs

--- Services/MainService.cs
+++ Services/MainService.cs
@@ -1,12 +1,9 @@
 ﻿using ContextExample.Data;
 using System;
+using System.ComponentModel;
 
 namespace ContextExample.Services;
 
-/// <summary>
-///     You would need to inject your interfaces here to execute the methods in Invoke()
-///     See the commented out code as an example
-/// </summary>
 public class MainService : IMainService
 {
     private readonly IContext _context;
@@ -22,7 +19,50 @@ public class MainService : IMainService
         // 1. select by id
         // 2. select by title 
         // 3. find movie by title
-        var movie = _context.GetById(1);
-        Console.WriteLine($"Your movie is {movie.Title}");
+        Console.WriteLine("1. Choose by ID\n2. Choose by Title\n3. Search Movies by Title");
+        Console.Write("Selection option please\n>");
+        
+        var input = Console.ReadLine();
+        
+        switch (input) {
+            case "1":
+                Console.Write("Enter movies ID.\n>");
+                var isValid = Int32.TryParse(Console.ReadLine(), out int userID);
+                if (isValid)
+                {
+                    var movie = _context.GetById(userID);
+
+                    Console.WriteLine($"Your movie is {movie.Title}");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Movie ID. Try again please.");
+                }
+                break;
+            
+            case "2":
+                Console.Write("Enter movies name.\n>");
+                var movieTitle = _context.GetByTitle(Console.ReadLine());
+                if (movieTitle == null)
+                {
+                    Console.Write("Incorrect Movie Title. Try again please."); 
+                    break;
+                }
+                Console.WriteLine($"{movieTitle.Title} has been found.");
+                break;
+           
+            case "3":
+                Console.Write("Select movie via search option.\n>");
+                var search = Console.ReadLine();
+                var movies = _context.FindMovie(search);
+                foreach(var mov in movies)
+                {
+                    Console.WriteLine(mov.Title);
+                }
+                break;
+            
+            default:
+                break;
+        }
     }
 }
